730af78230d06665b8aa7e904a9289f55ab085e9
Glenn Spinks <glennspinks@gmail..com>
2015-06-07 20:37:40 +0800

    Powershell List Folder Contents to File

---
+++ filesForQuery.ps1
@@ -0,0 +1,15 @@
+﻿
+#Refresh objects
+$files = $null;
+$outfile = $null;
+
+#Grab filenames without extensions
+$files = Get-ChildItem -File | Select-Object basename
+
+#Build up string
+foreach($file in $files){
+    $outfile += $file.BaseName + ', '; 
+}
+
+#Bazinga
+'(' + $outfile.Substring(0, $outfile.Length-2) + ')' | Out-File -FilePath output.csv
